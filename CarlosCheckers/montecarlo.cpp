2550594f6adff07b845a3c0abb6554039f447752
Garviell <faustusxi@gmail.com>
2015-03-22 23:09:04 +0000

    MADE STUFF IN MC

--- CarlosCheckers/montecarlo.cpp
+++ CarlosCheckers/montecarlo.cpp
@@ -1,1 +1,21 @@
 #include "montecarlo.h"
+
+MonteCarlo::~MonteCarlo(){
+	clearTree();
+}
+
+void MonteCarlo::clearTree(){
+	clearTree(root);
+}
+
+void MonteCarlo::clearTree(NodePtr node){
+	if (!node){
+		for (unsigned int i = 0; i < node->children.size(); i++){
+			clearTree(node->children[i]);
+		}
+		delete node;
+	}
+}
+double MonteCarlo::evaluationUCB1(NodePtr node){
+	return (node->win_count / (double)node->sim_count) + C * (sqrt(log(tsim_count) / (double)node->sim_count));
+}
--- CarlosCheckers/montecarlo.h
+++ CarlosCheckers/montecarlo.h
@@ -1,3 +1,21 @@
 #include "checkers.h"
-
+#include <cmath>
+#include "node.h"
+static double C  = sqrt(2);
 /* Uses the checkers checker board logic tools to generate moves and use them in its search */
+class MonteCarlo{
+public:
+	MonteCarlo(){ tsim_count = 0; root = NULL; }
+	~MonteCarlo();
+	void search();
+	void clearTree();
+	
+private:
+	NodePtr root;
+	double evaluationUCB1(NodePtr node);
+	void expand(NodePtr node);
+	void simulation(NodePtr node);
+	void clearTree(NodePtr node);
+	
+	int tsim_count;
+};
