6dc4b03220ddc52dd391e578a09757a82bf004fe
kirnih <hinrik11@ru.is>
2015-03-25 22:05:17 +0000

    Adding the test folder

---
+++ CarlosCheackersTests/TestBoard.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "..\CarlosCheckers\board.h"
+#include "..\CarlosCheckers\board.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CarlosCheackersTests
+{		
+	TEST_CLASS(Board)
+	{
+	public:
+		
+		TEST_METHOD(BoardConstructor)
+		{
+			Board gameBoard;
+			Assert::AreEqual(0, gameBoard.);
+			// TODO: Your test code here
+		}
+
+	};
+}
