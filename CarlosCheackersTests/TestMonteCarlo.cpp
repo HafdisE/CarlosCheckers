4d1297796fae960d483e7e9326b4cdfbc715c401
HafdisE <hafdiseh@gmail.com>
2015-03-30 14:06:06 +0000

    Test for debugging mc search added.

--- CarlosCheackersTests/TestMonteCarlo.cpp
+++ CarlosCheackersTests/TestMonteCarlo.cpp
@@ -1,7 +1,6 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
 #include "..\CarlosCheckers\montecarlo.h"
-
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace CarlosCheckersTests
@@ -11,10 +10,17 @@ namespace CarlosCheckersTests
 
 	};
 
-	TEST_CLASS(TestBoard)
+	TEST_CLASS(TestCarloSearch)
 	{
 	public:
-
+		TEST_METHOD(TestSearch) {
+			Checkers::setPlayer(WHITE);
+			MonteCarlo mc;
+			int playnow = 0;
+			char str[125];
+			mc.search(5, &playnow, str);
+		}
+			
 
 	};
 }
